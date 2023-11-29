49c1a40d0f95a54bdb970ee0eab06f1e8cead0dd
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-11-30 00:45:59 +0200

    Add project files.

---
+++ IteratorAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using IteratorAndCompositeExercise.Menus;
+using System.Collections;
+using System.Drawing;
+
+namespace IteratorAndCompositeExercise
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of menu classes
+            PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+            DinerMenu dinerMenu = new DinerMenu();
+
+            // Create a Waitress and pass the menus to it
+            Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu);
+
+            // Print the combined menu
+            waitress.PrintMenu();
+
+        }
+    }
+}
---
+++ IteratorAssignment/Waitress.cs
@@ -0,0 +1,47 @@
+﻿using IteratorAndCompositeExercise.InterfaceIterator;
+using IteratorAndCompositeExercise.Menus;
+using static IteratorAndCompositeExercise.Menus.ModelforMenus;
+
+namespace IteratorAndCompositeExercise
+{
+    public class Waitress
+    {
+        // Declare instances of PancakeHouseMenu and DinerMenu
+        PancakeHouseMenu pancakeHouseMenu;
+        DinerMenu dinerMenu;
+
+
+        // Constructor to initialize the Waitress with menus
+        public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu)
+        {
+            this.pancakeHouseMenu = pancakeHouseMenu;
+            this.dinerMenu = dinerMenu;
+        }
+        public void PrintMenu()
+        {
+            // Create iterators for PancakeHouseMenu and DinerMenu
+            IIterator pancakeIterator = pancakeHouseMenu.CreateIterator();
+            IIterator dinerIterator = dinerMenu.CreateIterator();
+
+            // Print Breakfast Menus
+            Console.WriteLine("MENU\n---\nBREAKFAST");
+            PrintMenu(pancakeIterator);
+
+            // Print lunch menu
+            Console.WriteLine("\nLUNCH");
+            PrintMenu(dinerIterator);
+        }
+        public void PrintMenu(IIterator iterator)
+        {
+            while (iterator.HasNext())
+            
+            {
+                MenuItem menuItem = (MenuItem)iterator.Next();
+                Console.Write(menuItem.GetName() + ", ");
+                Console.Write(menuItem.GetPrice() + " -- ");
+                Console.WriteLine(menuItem.GetDescription());
+            }
+        }
+
+    }
+}
